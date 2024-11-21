418ebc26880c5958f515595cf725ad349d7680c9
s230276 <gimpo.s230276@ggh.goe.go.kr>
2024-11-21 11:39:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,21 @@ col1,col2,col3 = st.columns(3)
 
 # 3. 데이터시각화
 
+with col1:
+      st.subheader('데이터시각화1')
+      st.image('____________' )   # 이미지 불러오기
+    col1:
+      st.subheader('데이터시각화1')
+      st.image('____________' )   # 이미지 불러오기
+  
 # 4. 모델 활용
 
+with col2:
+    st.subheader('사용자 정보 입력')
+    a = st.selectbox('나이는 몇살입니까?',[''])
+    b = st.selectbox('성별은 무엇인가요?',['Low','Medium','High'])
+    c = st.selectbox('bmi 수치는 몇인가요?',[''])
+    d = st.selectbox('자녀는 몇명인가요?',[''])
+    e = st.selectbox('흡연을 한적이 있습니까?',['Private','Public'])
+    f = st.selectbox('거주 지역은 어디인가요?',[''])
+   
