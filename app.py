47e26cb91b21fe70af60a7b98abfb8dfeeea78b3
s230276 <gimpo.s230276@ggh.goe.go.kr>
2024-11-21 11:46:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,10 +27,19 @@ with col1:
 
 with col2:
     st.subheader('사용자 정보 입력')
-    a = st.selectbox('나이는 몇살입니까?',[''])
-    b = st.selectbox('성별은 무엇인가요?',['Low','Medium','High'])
-    c = st.selectbox('bmi 수치는 몇인가요?',[''])
-    d = st.selectbox('자녀는 몇명인가요?',[''])
-    e = st.selectbox('흡연을 한적이 있습니까?',['Private','Public'])
-    f = st.selectbox('거주 지역은 어디인가요?',[''])
+    a = st.selectbox('나이는 몇살입니까?',value=0)
+    b = st.selectbox('성별은 무엇인가요?',['female,male'])
+    c = st.selectbox('bmi 수치는 몇인가요?',value=22.5)
+    d = st.selectbox('자녀는 몇명인가요?',value=0)
+    e = st.selectbox('흡연을 한적이 있습니까?',['yes,no'])
+    f = st.selectbox('거주 지역은 어디인가요?',['southwest,southeast,northwest,northeast'])
+
+# 5. 모델 설명
+
+with col3:
+    st.subheader('모델 설명')
+    st.write('기계학습 알고리즘 - 선형 회귀')
+    st.write('학습 데이터 출처 : https://www.kaggle.com/code/ruslankl/health-care-cost-prediction-w-linear-regression/input')
+    st.write('훈련 데이터 : 개')
+    st.write('테스트 데이터 : 개')
    
