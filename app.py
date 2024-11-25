2e6fa5634a0a72ce2afd79ce25281508db424fef
s230276 <gimpo.s230276@ggh.goe.go.kr>
2024-11-25 14:22:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ with col1:
 with col2:
     st.subheader('정보 입력')
     a = st.number_input('나이는 몇살입니까?',value=0)
-    b = st.selectbox('성별은 무엇인가요?',['female,male'])
+    b = st.selectbox('성별은 무엇인가요?(여성=0,남성=1)',[0,1])
     c = st.number_input('bmi 수치는 몇인가요?',value=22.5)
     d = st.number_input('자녀는 몇명인가요?',value=0)
-    e = st.selectbox('흡연을 한적이 있습니까?',['yes,no'])
-    f = st.selectbox('거주 지역은 어디인가요?',['southwest,southeast,northwest,northeast'])
+    e = st.selectbox('흡연을 한적이 있습니까?(X=0,O=1)',[0,1])
+    f = st.selectbox('거주 지역은 어디인가요?(남동,남서,동서',[0,1,2,3])
 # 5. 모델 설명
 with col3:
     st.subheader('모델 설명')
@@ -28,3 +28,8 @@ with col3:
     st.write('학습 데이터 출처 : https://www.kaggle.com/code/ruslankl/health-care-cost-prediction-w-linear-regression/input')
     st.write('훈련 데이터 : 개')
     st.write('테스트 데이터 : 개')
+      
+if st.button('점수 예측'):
+    input_data = [[a,b,c,d,e,f]]
+    p = model.predict(input_data)
+    st.write('인공지능이 예상한 당신의 점수는',p)
