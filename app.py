a18b2503664231868e416478a00f45505cc2bcdf
s230276 <gimpo.s230276@ggh.goe.go.kr>
2024-11-25 14:47:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 import streamlit as st
 # 1. 기계학습 모델 파일 로드
 import joblib
-model = joblib.load('linear_regression_model (2).pkl')
+model = joblib.load('linear_regression_model__2_.pkl')
 # 2. 모델 설명
-st.title('선형 회귀를 통한 의료비 예측')
+st.title('선형 linear_regression_model__2_.pkl 통한 의료비 예측')
 st.subheader('의료비를 어떻게 예측할 수 있을까?')
 col1,col2,col3 = st.columns(3)  
 # 3. 데이터시각화
